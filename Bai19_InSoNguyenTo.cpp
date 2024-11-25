464fa22abc490c891a6bdeb671ccb2fb45bd3690
hphuccntt <phuc54212420014@bachkhoasaigon.edu.vn>
2024-11-25 14:31:12 +0700

    sua bai 19

--- Bai19_InSoNguyenTo.cpp
+++ Bai19_InSoNguyenTo.cpp
@@ -18,7 +18,7 @@ int main()
 		if (dem==2)
 		{
 			cout << m << " ";
-		}
+		
 		
 	}
 	return 0;
@@ -26,5 +26,46 @@ int main()
 
 
 
+///
+/// ham kiem tra so nguyen to hay ko
+/// 
+/// so can kiem trea
+/// true la so nguyen
+/// false ko phai so nguyen
+/// 
+
+bool kiemtraSNT(int s) 
+{
+	int dem = 0;
+	for (int i = 1; i <= s; i++)
+	{
+		if (s % i == 0) {
+			dem++;
+		}
+	}
+	if (dem == 2)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+
+
+
+int main()
+{
+	cout << " Kiem Tra Phai So Nguyen To khong\n";
+	int so;
+	cout << " Nhap So: "; cin >> so;
+	for (int m = 0; m <= so; m++)
+	{
+		if (kiemtraSNT(m) == true) cout << m << " ";
+	}
+	return 0;
+}
 
 
