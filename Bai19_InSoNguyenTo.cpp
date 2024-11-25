babb6ca68e82dbf7e540684322cf3f3b5b8314e6
hphuccntt <phuc54212420014@bachkhoasaigon.edu.vn>
2024-11-25 15:35:08 +0700

    sua bai 19..,

--- Bai19_InSoNguyenTo.cpp
+++ Bai19_InSoNguyenTo.cpp
@@ -2,70 +2,82 @@
 using namespace std;
 int main()
 {
-	int so;
-	cout << "nhap so: "; cin >> so;
-	int dem = 0;
-	for (int m = 1; m <= so; m++)
+	/*
 	{
-		dem = 0;
-		for (int i = 1; i <= so; i++)
+		int so;
+		cout << "nhap so: "; cin >> so;
+		int dem = 0;
+		for (int m = 1; m <= so; m++)
 		{
-			if (m % i == 0)
+			dem = 0;
+			for (int i = 1; i <= so; i++)
 			{
+				if (m % i == 0)
+				{
+					dem++;
+				}
+			}
+			if (dem==2)
+			{
+				cout << m << " ";
+
+
+		}
+		return 0;
+	}*/
+	///
+	/// ham kiem tra so nguyen to hay ko
+	/// 
+	/// so can kiem true
+	/// true la so nguyen
+	/// false ko phai so nguyen
+	/// 
+
+
+	bool kiemtraSNT(int s) {
+		int dem = 0;
+		for (int i = 1; i <= s; i++)
+		{
+			if (s % i == 0) {
 				dem++;
 			}
 		}
-		if (dem==2)
+		if (dem == 2)
 		{
-			cout << m << " ";
-		
-		
-	}
-	return 0;
-}
-
-
-
-///
-/// ham kiem tra so nguyen to hay ko
-/// 
-/// so can kiem trea
-/// true la so nguyen
-/// false ko phai so nguyen
-/// 
-
-bool kiemtraSNT(int s) 
-{
-	int dem = 0;
-	for (int i = 1; i <= s; i++)
-	{
-		if (s % i == 0) {
-			dem++;
+			return true;
+		}
+		else
+		{
+			return false;
 		}
 	}
-	if (dem == 2)
-	{
-		return true;
-	}
-	else
-	{
+
+
+
+	bool kiemtraSCP(int x) {
+		if (sqrt(x) == (int)sqrt(x)) return true;
 		return false;
 	}
-}
 
-
-
-
-int main()
-{
-	cout << " Kiem Tra Phai So Nguyen To khong\n";
-	int so;
-	cout << " Nhap So: "; cin >> so;
-	for (int m = 0; m <= so; m++)
+	int main()
 	{
-		if (kiemtraSNT(m) == true) cout << m << " ";
+		cout << " Kiem Tra Phai So Nguyen To khong\n";
+		int so;
+		cout << " Nhap So: "; cin >> so;
+		for (int m = 1; m <= so; m++)
+		{
+			if (kiemtraSNT(m) == true) cout << m << " ";
+		}
+
+		cout << endl;
+
+		for (int i = 1; i < 100; i++)
+		{
+			if (kiemtraSCP(i) == true) cout << i << " ";
+		}
+		return 0;
 	}
-	return 0;
 }
+
 
 
